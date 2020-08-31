e8e791fbe31d99d7099b90bb843b5deb921d2f4b
RomuloRamos <romulo.ramos@gee.inatel.br>
2020-08-31 19:01:05 -0400

    Aplicacao do useEffec

---
+++ React/UdemyCourse/ModernReactWithRedux2020/widgets/src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Accordion from './components/Accordion';
+import Search from './components/Search';
+
+
+
+const items = [
+    {
+        title: 'What is React?',
+        content: 'React is a front end javascript framework'
+    },
+    {
+        title: 'Why use React?',
+        content: 'React is a favorite JS library among engineers'
+    },
+    {
+        title: 'How do you use React?',
+        content: 'You use React by creating components  '
+    },
+];
+
+const App = (props)=> {
+    return (
+        <div>
+            <Search search= {items}/>
+        </div>
+    );
+}
+
+export default App;
---
+++ React/UdemyCourse/ModernReactWithRedux2020/widgets/src/components/Search.js
@@ -0,0 +1,111 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const Search = ({search}) => {
+
+    const [term, setTerm] = useState('');
+    // const [debouncedTerm, setDebouncedTerm] = useState(term); 
+    const [results, setResults] = useState([]);
+
+    // console.log(results);
+
+    // useEffect(()=>{
+    //     const timerId = setTimeout(()=>{
+    //         setDebouncedTerm(term);
+    //     },1000);
+
+    //     return(()=>{
+    //         clearTimeout(timerId);
+    //     });
+    // }, [term]);
+  
+    // useEffect(()=>{
+    //     const search = async () =>{
+    //         const {data} = await axios.get('https://en.wikipedia.org/w/api.php', {
+    //             params: {
+    //                 action: 'query',
+    //                 list: 'search',
+    //                 origin: '*',
+    //                 format: 'json',
+    //                 srsearch: debouncedTerm,
+    //             },
+    //         });
+    //         setResults(data.query.search);
+    //     };
+    //     search();
+    // },[debouncedTerm]);
+
+    useEffect( ()=>{
+        // console.log("Chamando função de useEffect para registrar a função de reação ao proximo RENDER");
+
+        // return ()=>{
+        //     console.log("Função que foi registrada com o useEffect");
+        // };
+        console.log('useEffect called!');
+        const search = async () =>{
+            const {data} = await axios.get('https://en.wikipedia.org/w/api.php', {
+                params: {
+                    action: 'query',
+                    list: 'search',
+                    origin: '*',
+                    format: 'json',
+                    srsearch: term,
+                },
+            });
+            setResults(data.query.search);
+        };
+        if(term && !results.length) {
+            search();
+        } else {    
+            const timeOutId = setTimeout(()=>{
+                if(term  ) search();
+            },1000);
+    
+            return ()=>{
+                clearTimeout(timeOutId);
+            };
+        }
+
+    }, [term]);
+
+    const renderedResults = results.map((result) => {
+        return (
+            <div key={result.pageid} className="item">
+                <div className="right floated content">
+                    <a 
+                        className="ui button"
+                        href={`https://en.wikipedia.org?curid=${result.pageid}`}
+                    >
+                        Go
+                    </a>
+                </div>
+                <div className="content">
+                    <div className="header">
+                        {result.title}
+                    </div>
+                    <span dangerouslySetInnerHTML={{__html:result.snippet}}></span>
+                </div>
+            </div>
+        );
+    }) 
+
+    return (
+        <div>
+            <div className="ui form">
+                <div className="field">
+                    <label> Enter Seach Term</label>
+                    <input
+                        value={term}
+                        onChange={e=>setTerm(e.target.value)} 
+                        type="text" 
+                        className="input"/>
+                </div>
+            </div>
+            <div className="ui celled list">
+                {renderedResults}
+            </div>
+        </div>
+    );
+}
+
+export default Search;
