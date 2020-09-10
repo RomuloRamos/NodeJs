3218ac79cf051710df4c64858fcf6e7474f61d5b
RomuloRamos <romulo.ramos@gee.inatel.br>
2020-09-09 22:55:10 -0400

    Aula sobre useEfect

--- React/UdemyCourse/ModernReactWithRedux2020/widgets/src/components/Search.js
+++ React/UdemyCourse/ModernReactWithRedux2020/widgets/src/components/Search.js
@@ -3,45 +3,24 @@ import axios from 'axios';
 
 const Search = ({search}) => {
 
-    const [term, setTerm] = useState('');
-    // const [debouncedTerm, setDebouncedTerm] = useState(term); 
+    const [term, setTerm] = useState('');//<-Como eu deixei vazio, isso gera um erro na linha 37, ao chamar o metodo search()
+    const [debouncedTerm, setDebouncedTerm] = useState(term); 
     const [results, setResults] = useState([]);
 
     // console.log(results);
 
-    // useEffect(()=>{
-    //     const timerId = setTimeout(()=>{
-    //         setDebouncedTerm(term);
-    //     },1000);
+    useEffect(()=>{
+        const timerId = setTimeout(()=>{
+            console.log("setDebouncedTerm:",term);
+            setDebouncedTerm(term);
+        },1000);
 
-    //     return(()=>{
-    //         clearTimeout(timerId);
-    //     });
-    // }, [term]);
+        return(()=>{
+            clearTimeout(timerId);
+        });
+    }, [term]);
   
-    // useEffect(()=>{
-    //     const search = async () =>{
-    //         const {data} = await axios.get('https://en.wikipedia.org/w/api.php', {
-    //             params: {
-    //                 action: 'query',
-    //                 list: 'search',
-    //                 origin: '*',
-    //                 format: 'json',
-    //                 srsearch: debouncedTerm,
-    //             },
-    //         });
-    //         setResults(data.query.search);
-    //     };
-    //     search();
-    // },[debouncedTerm]);
-
-    useEffect( ()=>{
-        // console.log("Chamando função de useEffect para registrar a função de reação ao proximo RENDER");
-
-        // return ()=>{
-        //     console.log("Função que foi registrada com o useEffect");
-        // };
-        console.log('useEffect called!');
+    useEffect(()=>{
         const search = async () =>{
             const {data} = await axios.get('https://en.wikipedia.org/w/api.php', {
                 params: {
@@ -49,24 +28,47 @@ const Search = ({search}) => {
                     list: 'search',
                     origin: '*',
                     format: 'json',
-                    srsearch: term,
+                    srsearch: debouncedTerm,
                 },
             });
+            // console.log("setResults: ",data.query.search);
             setResults(data.query.search);
         };
-        if(term && !results.length) {
-            search();
-        } else {    
-            const timeOutId = setTimeout(()=>{
-                if(term  ) search();
-            },1000);
-    
-            return ()=>{
-                clearTimeout(timeOutId);
-            };
-        }
+        if(debouncedTerm)search();
+    },[debouncedTerm]);
 
-    }, [term]);
+    // useEffect( ()=>{
+    //     // console.log("Chamando função de useEffect para registrar a função de reação ao proximo RENDER");
+
+    //     // return ()=>{
+    //     //     console.log("Função que foi registrada com o useEffect");
+    //     // };
+    //     console.log('useEffect called!');
+    //     const search = async () =>{
+    //         const {data} = await axios.get('https://en.wikipedia.org/w/api.php', {
+    //             params: {
+    //                 action: 'query',
+    //                 list: 'search',
+    //                 origin: '*',
+    //                 format: 'json',
+    //                 srsearch: term,
+    //             },
+    //         });
+    //         setResults(data.query.search);
+    //     };
+    //     if(term && !results.length) {
+    //         search();
+    //     } else {    
+    //         const timeOutId = setTimeout(()=>{
+    //             if(term  ) search();
+    //         },1000);
+    
+    //         return ()=>{
+    //             clearTimeout(timeOutId);
+    //         };
+    //     }
+
+    // }, [term]);
 
     const renderedResults = results.map((result) => {
         return (
