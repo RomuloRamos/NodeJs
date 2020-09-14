0757885e00809d39fbe0f8782af4cc6f5020abcc
RomuloRamos <romulo.ramos@gee.inatel.br>
2020-09-14 17:39:06 -0400

    aula 168

--- React/UdemyCourse/ModernReactWithRedux2020/widgets/src/App.js
+++ React/UdemyCourse/ModernReactWithRedux2020/widgets/src/App.js
@@ -1,8 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Accordion from './components/Accordion';
 import Search from './components/Search';
-
-
+import Dropdown from './components/Dropdown';
 
 const items = [
     {
@@ -19,10 +18,32 @@ const items = [
     },
 ];
 
+const options = [
+    {
+        label: "Select Color Red",
+        value: "red"
+    },
+    {
+        label: "Select Color Green",
+        value: "green"
+    },
+    {
+        label: "Select Color Blue",
+        value: "blue"
+    },
+]
+
 const App = (props)=> {
+    const [selectedColor, setSelectedColor] = useState(options[0]);
+
     return (
         <div>
-            <Search search= {items}/>
+            {/* <Search search= {items}/> */}
+            <Dropdown
+                selected = {selectedColor}
+                onSelectedChange={setSelectedColor}
+                options = {options} 
+            />
         </div>
     );
 }
