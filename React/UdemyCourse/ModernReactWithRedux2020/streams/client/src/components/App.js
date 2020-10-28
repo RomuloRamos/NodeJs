b8ca4d8a25fd0c99dd20119e9fea9eb3141e5246
RomuloRamos <romulo.ramos@gee.inatel.br>
2020-10-27 20:14:15 -0400

    Adding RTMP Server to kiding

--- React/UdemyCourse/ModernReactWithRedux2020/streams/client/src/components/App.js
+++ React/UdemyCourse/ModernReactWithRedux2020/streams/client/src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Router } from 'react-router-dom';
+import { Route, Router, Switch } from 'react-router-dom';
 import Streams from './streams';
 import Header from './Header';
 import history from '../history';
@@ -12,11 +12,13 @@ const App = ()=>{
             <Router history={history}>
                 <div>
                     <Header></Header>
-                    <Route path='/' exact component={Streams.list}/>
-                    <Route path='/streams/new' exact component={Streams.create}/>
-                    <Route path='/streams/delete/:id' exact component={Streams.delete}/>
-                    <Route path='/streams/show' exact component={Streams.show}/>
-                    <Route path='/streams/edit/:id' exact component={Streams.edit}/> 
+                    <Switch>
+                        <Route path='/' exact component={Streams.list}/>
+                        <Route path='/streams/new' exact component={Streams.create}/>
+                        <Route path='/streams/delete/:id' exact component={Streams.delete}/>
+                        <Route path='/streams/:id' exact component={Streams.show}/>
+                        <Route path='/streams/edit/:id' exact component={Streams.edit}/> 
+                    </Switch>
                 </div>  
             </Router>
         </div>
--- React/UdemyCourse/ModernReactWithRedux2020/streams/client/src/components/streams/StreamShow.js
+++ React/UdemyCourse/ModernReactWithRedux2020/streams/client/src/components/streams/StreamShow.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {featchStream} from '../../actions';
 
+class StreamShow extends React.Component {
 
-const StreamShow = () => {
-    return (
-        <div>StreamShow</div>
-    );
+    componentDidMount(){
+        this.props.featchStream(this.props.match.params.id);
+    }
+    
+    render(){
+        if(!this.props.stream){
+            return <div>Loading...</div>
+        }
+
+        const {title, description} = this.props.stream;
+        return (
+            <div>
+                <h1>{title}</h1>
+                <h5>description</h5>
+            </div>
+        );
+    }
 };
 
-export default StreamShow;
+
+const mapStateToProps = (state, ownProps) => {
+    return {stream: state.streams[ownProps.match.params.id]};
+};
+
+export default connect(mapStateToProps, {featchStream})(StreamShow);
