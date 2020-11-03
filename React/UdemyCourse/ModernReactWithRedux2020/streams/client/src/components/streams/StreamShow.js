b02f54b3b9062e065e0acefec7aec963635679c5
RomuloRamos <romulo.ramos@gee.inatel.br>
2020-11-03 15:55:13 -0400

    Using flv library to import a video player to page

--- React/UdemyCourse/ModernReactWithRedux2020/streams/client/src/components/streams/StreamShow.js
+++ React/UdemyCourse/ModernReactWithRedux2020/streams/client/src/components/streams/StreamShow.js
@@ -1,11 +1,37 @@
 import React from 'react';
+import flv from 'flv.js';
 import {connect} from 'react-redux';
 import {featchStream} from '../../actions';
 
 class StreamShow extends React.Component {
 
+    constructor(props){
+        super(props);
+
+        this.videoRef = React.createRef();
+    }
+
     componentDidMount(){
-        this.props.featchStream(this.props.match.params.id);
+        const {id} = this.props.match.params;
+
+        this.props.featchStream(id);
+        this.buildPlayer();
+    }
+
+    buildPlayer(){
+        if(this.player || !this.props.stream){
+            return;
+        }
+        
+        const {id} = this.props.match.params;
+        this.player = flv.createPlayer(
+            {
+                type: 'flv',
+                url: `http://localhost:8000/live/${id}.flv`
+            }
+        );
+        this.player.attachMediaElement(this.videoRef.current); 
+        this.player.load(); 
     }
     
     render(){
@@ -16,7 +42,8 @@ class StreamShow extends React.Component {
         const {title, description} = this.props.stream;
         return (
             <div>
-                <h1>{title}</h1>
+                <video ref={this.videoRef} style={{width:'100%'}} controls={true}/>
+                 <h1>{title}</h1>
                 <h5>description</h5>
             </div>
         );
